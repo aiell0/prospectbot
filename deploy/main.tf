19125b60ef8f6b4b13210d82af65b784ed51f81c
aiell0 <larello732@gmail.com>
2019-06-21 02:05:23 +0000

    Add tag to instance

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-	ami = "ami-9887c6e7"
-	instance_type = "t2.micro"
+  ami           = "ami-9887c6e7"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
