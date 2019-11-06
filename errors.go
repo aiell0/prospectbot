d8dba591f17ccbdf86a9792a64a51acaad7e75d6
prospectbot <lawrence.aiello@blockforgecapital.com>
2019-11-06 12:43:06 +0000

    PROS-16 currently getting Invalid Ciphertext exceptions.

--- errors.go
+++ errors.go
@@ -2,34 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws/awserr"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/kms"
 	log "github.com/sirupsen/logrus"
 )
-
-type Attributes struct {
-	ApproximateReceiveCount          string
-	SentTimestamp                    string
-	SenderId                         string
-	ApproximateFirstReceiveTimestamp string
-}
-
-type Record struct {
-	MessageId         string
-	ReceiptHandle     string
-	Body              string
-	Attributes        Attributes
-	MessageAttributes struct{}
-	Md5OfBody         string
-	EventSource       string
-	EventSourceARN    string
-	AwsRegion         string
-}
-
-type SQSEvent struct {
-	Records []Record
-}
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -43,8 +26,63 @@ func init() {
 	}
 }
 
-func errMsg(ctx context.Context, event SQSEvent) (string, error) {
-	log.Info(event)
+func getSlackToken() {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+
+	// Set the AWS Region that the service clients should use
+	cfg.Region = endpoints.UsEast1RegionID
+	fmt.Println(os.Getenv("SLACK_TOKEN"))
+	svc := kms.New(cfg)
+	encryptionContext := make(map[string]string)
+	encryptionContext["PARAMETER_ARN"] = "arn:aws:ssm:us-east-1:385445628596:parameter/slack/access-token"
+	input := &kms.DecryptInput{
+		CiphertextBlob:    []byte(os.Getenv("SLACK_TOKEN")),
+		EncryptionContext: encryptionContext,
+	}
+
+	req := svc.DecryptRequest(input)
+	result, err := req.Send()
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case kms.ErrCodeNotFoundException:
+				fmt.Println(kms.ErrCodeNotFoundException, aerr.Error())
+			case kms.ErrCodeDisabledException:
+				fmt.Println(kms.ErrCodeDisabledException, aerr.Error())
+			case kms.ErrCodeInvalidCiphertextException:
+				fmt.Println(kms.ErrCodeInvalidCiphertextException, aerr.Error())
+			case kms.ErrCodeKeyUnavailableException:
+				fmt.Println(kms.ErrCodeKeyUnavailableException, aerr.Error())
+			case kms.ErrCodeDependencyTimeoutException:
+				fmt.Println(kms.ErrCodeDependencyTimeoutException, aerr.Error())
+			case kms.ErrCodeInvalidGrantTokenException:
+				fmt.Println(kms.ErrCodeInvalidGrantTokenException, aerr.Error())
+			case kms.ErrCodeInternalException:
+				fmt.Println(kms.ErrCodeInternalException, aerr.Error())
+			case kms.ErrCodeInvalidStateException:
+				fmt.Println(kms.ErrCodeInvalidStateException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func errMsg(ctx context.Context, sqsEvent events.SQSEvent) (string, error) {
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+	}
+	getSlackToken()
 	// send to slack
 	// exit lambda runtime
 	return "SUCCESS", nil
