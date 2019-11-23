8c76dd21d0f4f904677362d55e9bb627e532400e
prospectbot <lawrence.aiello@blockforgecapital.com>
2019-11-23 20:29:29 +0000

    PROS-3 removed dead filed

--- deploy/stage/services/webserver-cluster/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "elb_dns_name" {
-  value = "${aws_elb.example.dns_name}"
-}
--- deploy/stage/services/webserver-cluster/user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat > index.html <<EOF
-<h1>Hello, world</h1>
-<p>DB Address: ${db_address}</p>
-<p>DB Port: ${db_port}</p>
-EOF
-
-nohup busybox httpd -f -p "${server_port}" &
